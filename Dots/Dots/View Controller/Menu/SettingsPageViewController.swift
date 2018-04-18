8e4fd07326a5c4e1809a009b43f2987693bb95dd
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-04-18 12:49:46 -0300

    Created Menu section

---
+++ Dots/Dots/View Controller/Menu/SettingsPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsPageViewController.swift
+//  Dots
+//
+//  Created by Giovani Nascimento Pereira on 18/04/18.
+//  Copyright © 2018 Giovani Nascimento Pereira. All rights reserved.
+//
+
+import UIKit
+
+class SettingsPageViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLabel.transform = CGAffineTransform(rotationAngle: 0.25)
+    }
+
+}
